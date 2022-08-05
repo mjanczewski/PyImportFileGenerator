b024733a75b88617295f11b017b0aedc1ed93f07
Marek Janczewski <mrjanczewski@gmail.com>
2022-08-05 10:11:58 +0200

    Generowanie cen z rabatem

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
+from decimal import Decimal
+from numpy import mat
 import pandas as pd
+import math
 
-abc = pd.read_excel('test.xls')
+sandisk = pd.read_excel('test.xls')
 
-print(abc['Kod towaru'])
+
+sandisk.rename(columns={'Kod towaru': 'KOD', 'Cena': 'WARTOŚĆ'}, inplace=True)
+sandisk.insert(1, 'RODZAJ', int(3))
+sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ']*0.99, 2)
+sandisk['PRÓG'] = 0
+sandisk['TYPNB'] = 'N'
+sandisk['WALUTA'] = 'PLN'
+sandisk['RODZAJ CENY'] = 'DETALICZNA'
+sandisk['GRATIS'] = 'NIE'
+sandisk['RODZAJ LIMITU'] = 0
+sandisk['TYP LIMITU'] = 0
+sandisk['LIMIT'] = 0
+sandisk['TYP PROGU TRANSAKCJI'] = 0
+sandisk['PRÓG TRANSAKCJI'] = 0
+
+del sandisk['Kod u kontrahenta']
+del sandisk['Kontrahent']
+
+
+# sandisk.drop(['Kod u kontrahenta', 'Kontrahent'],
+#              index=False, axis=1, inplace=True)
+
+
+sandisk.to_excel('sandisk_mod.xls', index=False)
