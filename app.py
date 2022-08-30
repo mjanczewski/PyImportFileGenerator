a87fecd05f8194881f7e8133a7bb43ee14d5240d
Marek Janczewski <mrjanczewski@gmail.com>
2022-08-30 15:35:16 +0200

    oblicznie wartości w pętli

--- app.py
+++ app.py
@@ -2,93 +2,28 @@ from decimal import Decimal
 from numpy import mat
 import pandas as pd
 
+rabaty = [1, 0.99, 0.98, 0.97]
+licznik = 0
 
-sandisk = pd.read_excel('sandisk.xls')
-
-
-sandisk.rename(columns={'Kod towaru': 'KOD', 'Cena': 'WARTOŚĆ'}, inplace=True)
-sandisk.insert(1, 'RODZAJ', int(3))
-sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ'], 2)
-sandisk['PRÓG'] = 0
-sandisk['TYPNB'] = 'N'
-sandisk['WALUTA'] = 'PLN'
-sandisk['RODZAJ CENY'] = 'DETALICZNA'
-sandisk['GRATIS'] = 'NIE'
-sandisk['RODZAJ LIMITU'] = 0
-sandisk['TYP LIMITU'] = 0
-sandisk['LIMIT'] = 0
-sandisk['TYP PROGU TRANSAKCJI'] = 0
-sandisk['PRÓG TRANSAKCJI'] = 0
-
-# del sandisk['Kod u kontrahenta']
-# del sandisk['Kontrahent']
-
-
-sandisk.to_excel('sandisk_All.xls', index=False)
-
-sandisk = pd.read_excel('sandisk.xls')
-
-
-sandisk.rename(columns={'Kod towaru': 'KOD', 'Cena': 'WARTOŚĆ'}, inplace=True)
-sandisk.insert(1, 'RODZAJ', int(3))
-sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ']*0.99, 2)
-sandisk['PRÓG'] = 0
-sandisk['TYPNB'] = 'N'
-sandisk['WALUTA'] = 'PLN'
-sandisk['RODZAJ CENY'] = 'DETALICZNA'
-sandisk['GRATIS'] = 'NIE'
-sandisk['RODZAJ LIMITU'] = 0
-sandisk['TYP LIMITU'] = 0
-sandisk['LIMIT'] = 0
-sandisk['TYP PROGU TRANSAKCJI'] = 0
-sandisk['PRÓG TRANSAKCJI'] = 0
-
-# del sandisk['Kod u kontrahenta']
-# del sandisk['Kontrahent']
-
-
-sandisk.to_excel('sandisk_1_procent.xls', index=False)
-
-sandisk = pd.read_excel('sandisk.xls')
-
-
-sandisk.rename(columns={'Kod towaru': 'KOD', 'Cena': 'WARTOŚĆ'}, inplace=True)
-sandisk.insert(1, 'RODZAJ', int(3))
-sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ']*0.98, 2)
-sandisk['PRÓG'] = 0
-sandisk['TYPNB'] = 'N'
-sandisk['WALUTA'] = 'PLN'
-sandisk['RODZAJ CENY'] = 'DETALICZNA'
-sandisk['GRATIS'] = 'NIE'
-sandisk['RODZAJ LIMITU'] = 0
-sandisk['TYP LIMITU'] = 0
-sandisk['LIMIT'] = 0
-sandisk['TYP PROGU TRANSAKCJI'] = 0
-sandisk['PRÓG TRANSAKCJI'] = 0
-
-# del sandisk['Kod u kontrahenta']
-# del sandisk['Kontrahent']
-
-sandisk.to_excel('sandisk_2_procent.xls', index=False)
-
-sandisk = pd.read_excel('sandisk.xls')
-
-
-sandisk.rename(columns={'Kod towaru': 'KOD', 'Cena': 'WARTOŚĆ'}, inplace=True)
-sandisk.insert(1, 'RODZAJ', int(3))
-sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ']*0.97, 2)
-sandisk['PRÓG'] = 0
-sandisk['TYPNB'] = 'N'
-sandisk['WALUTA'] = 'PLN'
-sandisk['RODZAJ CENY'] = 'DETALICZNA'
-sandisk['GRATIS'] = 'NIE'
-sandisk['RODZAJ LIMITU'] = 0
-sandisk['TYP LIMITU'] = 0
-sandisk['LIMIT'] = 0
-sandisk['TYP PROGU TRANSAKCJI'] = 0
-sandisk['PRÓG TRANSAKCJI'] = 0
-
-# del sandisk['Kod u kontrahenta']
-# del sandisk['Kontrahent']
-
-sandisk.to_excel('sandisk_3_procent.xls', index=False)
+for i in rabaty:
+    sandisk = pd.read_excel('sandisk.xls')
+    sandisk.rename(columns={'Kod towaru': 'KOD',
+                   'Cena': 'WARTOŚĆ'}, inplace=True)
+    sandisk.insert(1, 'RODZAJ', int(3))
+    sandisk['WARTOŚĆ'] = round(sandisk['WARTOŚĆ']*i, 2)
+    sandisk['PRÓG'] = 0
+    sandisk['TYPNB'] = 'N'
+    sandisk['WALUTA'] = 'PLN'
+    sandisk['RODZAJ CENY'] = 'DETALICZNA'
+    sandisk['GRATIS'] = 'NIE'
+    sandisk['RODZAJ LIMITU'] = 0
+    sandisk['TYP LIMITU'] = 0
+    sandisk['LIMIT'] = 0
+    sandisk['TYP PROGU TRANSAKCJI'] = 0
+    sandisk['PRÓG TRANSAKCJI'] = 0
+    if licznik == 0:
+        sandisk.to_excel(f'sandisk_ALL.xls', index=False)
+        licznik += 1
+    else:
+        sandisk.to_excel(f'sandisk_{licznik}_procent.xls', index=False)
+        licznik += 1
